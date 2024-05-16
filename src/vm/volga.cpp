01467c9b9802ea8c879430945a8ecc5cf2bf025c
PaulQ-dev <paul-dev@mail.ru>
2024-05-16 20:58:53 +0300

    progress in vm's implementation

--- src/vm/volga.cpp
+++ src/vm/volga.cpp
@@ -8,9 +8,7 @@ string volga::helloWorld() {
     return "Hello C++";
 }
 
-/**
- ** false - success
- **/
+// false - success
 bool volga::loadRom(byte_vm rom[], int rom_len){
     _prog_count = _rom_start;
     int i = 0;
@@ -22,27 +20,99 @@ bool volga::loadRom(byte_vm rom[], int rom_len){
     if (i <= rom_len - 1) return false;
     return true;
 }
-/**
- ** false - success
- **/
+// false - success
 bool volga::loadRom(string rom_file){
     FILE* f = fopen(rom_file.c_str(), "r");
     int fsize = 0;
     while(true){
-        byte_vm b;
-        fread(&b,1,1,f);
+        cout<<fsize<<endl;
+        int b = fgetc(f);
+        if(b == EOF) break;
+        fsize++;
     }
+    byte_vm rom[fsize];
+    for(int i = 0; i < fsize; i++){
+        rom[i] = fgetc(f);
+    }
+    return loadRom(rom,fsize);
+}
+
+void volga::loadA(byte_vm value){
+    _accum = value;
+}
+void volga::loadA(addr_vm address){
+    _addr_buffer = _prog_count;
+    _prog_count = address;
+    read();
+    loadA(_buffer);
+}
+void volga::storeA(addr_vm address){
+    _addr_buffer = address;
+    _buffer = _accum;
+    write();
+}
+
+void volga::loadX(byte_vm value){
+    _x_reg = value;
+}
+void volga::loadX(addr_vm address){
+    _addr_buffer = _prog_count;
+    _prog_count = address;
+    read();
+    loadX(_buffer);
+}
+void volga::storeX(addr_vm address){
+    _addr_buffer = address;
+    _buffer = _x_reg;
+    write();
+}
+
+void volga::loadY(byte_vm value){
+    _y_reg = value;
+}
+void volga::loadY(addr_vm address){
+    _addr_buffer = _prog_count;
+    _prog_count = address;
+    read();
+    loadY(_buffer);
+}
+void volga::storeY(addr_vm address){
+    _addr_buffer = address;
+    _buffer = _y_reg;
+    write();
+}
+
+void volga::read(){
+    _buffer = _mem.getMemory(_addr_buffer);
+}
+void volga::readAddr(){
+    read();
+    _addr_buffer = _buffer;
+    read();
+    _addr_buffer = (_addr_buffer * 256) + _buffer;
+}
+void volga::write(){
+    _mem.setMemory(_addr_buffer, _buffer);
+}
+
+int volga::run(){
+    _prog_count = _rom_start;
+    
+    return 0;
 }
 
 volga::volga() {
     _mem = memory();
     _ram_start = 0x0000;
+    _ram_end = 0x7FFF;
     _rom_start = 0x9000;
+    _rom_end = 0xFFFF;
     _prog_count = 0x0000;
     _proc_flags = 0b00000000;
     _accum = 0x00;
     _x_reg = 0x00;
     _y_reg = 0x00;
+    _buffer = 0x00;
 }
 volga::~volga(){
     _mem.~memory();
--- src/vm/volga.h
+++ src/vm/volga.h
@@ -10,10 +10,17 @@ namespace paulqpro::volga{
     class volga
     {
     private:
-        addr_vm _ram_start, _rom_start, _prog_count;
-        byte_vm _proc_flags, _accum, _x_reg, _y_reg;
+        addr_vm _ram_start, _ram_end,
+                _rom_start, _rom_end,
+                _prog_count, _addr_buffer;
+        byte_vm _proc_flags, _accum, _x_reg, _y_reg, _buffer;
         memory _mem;
+        void loadA(byte_vm value), loadA(addr_vm address), storeA(addr_vm address);
+        void loadX(byte_vm value), loadX(addr_vm address), storeX(addr_vm address);
+        void loadY(byte_vm value), loadY(addr_vm address), storeY(addr_vm address);
+        void read(), readAddr(), write();
     public:
+        int run();
         bool loadRom(byte_vm rom[], int rom_len);
         bool loadRom(string rom_file);
         string helloWorld();
