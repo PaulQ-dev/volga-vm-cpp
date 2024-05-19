b0b8ef6617a318dcbf2162354611e5ee23632b9e
PaulQ-dev <paul-dev@mail.ru>
2024-05-19 03:04:24 +0300

    Added and implemented  necessary functions

--- src/vm/volga.h
+++ src/vm/volga.h
@@ -12,13 +12,17 @@ namespace paulqpro::volga{
     private:
         addr_vm _ram_start, _ram_end,
                 _rom_start, _rom_end,
+                _stack_start, _stack_end,
+                _cout, _cin, _cnl, 
                 _prog_count, _addr_buffer;
-        byte_vm _proc_flags, _accum, _x_reg, _y_reg, _buffer;
+        byte_vm _proc_flags, _accum, _x_reg, _y_reg, _buffer, _mem_buffer, _stack_ptr, _stack_buffer;
         memory _mem;
         void loadA(byte_vm value), loadA(addr_vm address), storeA(addr_vm address);
         void loadX(byte_vm value), loadX(addr_vm address), storeX(addr_vm address);
         void loadY(byte_vm value), loadY(addr_vm address), storeY(addr_vm address);
-        void read(), readAddr(), write();
+        void read(), readPC(), readAddr(), readAddrPC(), write();
+        void push(), pull();
+        byte_vm ror(byte_vm original), rol(byte_vm original);
     public:
         int run();
         bool loadRom(byte_vm rom[], int rom_len);
